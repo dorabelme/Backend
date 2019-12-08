8dd49c97e75473e3477612e9c49a786ecd04bcce
Dora Belme <contact.dorabelme@gmail.com>
2019-12-07 21:06:35 -0500

    Fix candidate

--- models/candidate.js
+++ models/candidate.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const Candidate = new Schema({
   handle: { type: String, required: true },
-  id: { type: Number, required: true }
+  id: { type: Object, required: true }
 });
-exports.Candidate = mongoose.model("Candidate", Candidate);
+
+exports.Candidate = mongoose.model("Candidate", Candidate, "qwe");
